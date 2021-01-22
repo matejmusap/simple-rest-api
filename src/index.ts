47c18db954f226f5341622fe04c928ff54bc469f
matejmusap <matej.musap@briteback.com>
2021-01-22 22:09:51 +0100

    refact sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,17 @@
 import bodyParser from 'body-parser';
-import express, { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import sequelize from './utils/sequelize';
-import { generateDocumentation } from './swagger';
-import { error404, error500, handle } from './utils/handlers';
+import express from 'express';
+import sequilazeInit from './models';
+import { error404, error500 } from './utils/handlers';
+import router from './routes';
 
 require('dotenv').config();
 
-(async () => await sequelize.sync({ logging: false }))();
-
-const getPaths = {
-  summary: 'Get',
-  produces: ['application/json'],
-  responses: {
-    200: { description: 'OK' },
-    400: { description: 'Bad request.' },
-    404: { description: 'Requested resource not found' },
-    500: { description: 'Internal server error' }
-  }
-};
-
-const paths = {
-  '/': {
-    get: getPaths
-  },
-  '/login': {
-    get: getPaths
-  },
-  '/register': {
-    get: getPaths
-  },
-  '/forgot': {
-    get: getPaths
-  }
-};
-
-const app = express();
+sequilazeInit();
 
 const port = process.env.PORT || 3000;
 
-const documentation = generateDocumentation(paths);
+const app = express();
 
-const router = Router();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -50,40 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use(error404);
 app.use(error500);
-
-router.use('/', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(documentation));
-router.get('/swagger', (_req, res) =>
-  res.send(JSON.stringify(documentation, null, 2))
-);
-
-router.get(
-  '/',
-  handle((_req, res, _next) => {
-    res.redirect('main!');
-  })
-);
-
-router.get(
-  '/login',
-  handle((_req, res, _next) => {
-    res.render('login');
-  })
-);
-
-router.get(
-  '/register',
-  handle((_req, res, _next) => {
-    res.render('register');
-  })
-);
-
-router.get(
-  '/forgot',
-  handle((_req, res, _next) => {
-    res.render('forgot');
-  })
-);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,22 @@
 import UserModel from './User';
+import { Model, ModelCtor, Sequelize } from 'sequelize';
+
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'test',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || ''
+});
+
+export const User = sequelize.define(UserModel.table, UserModel.Scheme, {
+  ...UserModel.options
+}) as ModelCtor<Model<any, any>>;
+
+const sequilazeInit = () => sequelize.sync();
 
 export { UserModel };
+
+export default sequilazeInit;
